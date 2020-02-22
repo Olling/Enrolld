b0267d465ee1ab1add09ac24d6ac316f3b3c9448
brondum <34370407+brondum@users.noreply.github.com>
2020-02-22 21:05:55 +0100

    #2: changed to use err

--- api/targets.go
+++ api/targets.go
@@ -3,20 +3,21 @@ package api
 import (
 	"fmt"
 	"net/http"
+
 	"github.com/Olling/Enrolld/output"
 )
 
 func getTargets(w http.ResponseWriter, r *http.Request) {
-	servers, getErr := output.GetInventory()
+	servers, err := output.GetInventory()
 
-	if getErr != nil {
-		fmt.Println(getErr)
+	if err != nil {
+		fmt.Println(err)
 		http.Error(w, http.StatusText(500), 500)
 	}
 
 	targets, targetsErr := output.GetTargetsInJSON(servers)
-	if targetsErr != nil {
-		fmt.Println(targetsErr)
+	if err = nil {
+		fmt.Println(err)
 		http.Error(w, http.StatusText(500), 500)
 		return
 	}
